53328e4c9871f7df8a5ba222cf3cb4ac937e3622
jmaciel12 <jmacielrampone@udesa.edu.ar>
2024-05-16 22:27:17 -0300

    guardado, me dejó de funcionar el servidor

--- JS/bucles.js
+++ JS/bucles.js
@@ -1,6 +1,46 @@
-function rectangulo(altura, ancho) {
-    var area = altura * ancho;
-    return area;
-}
+// Ejercicio 1
+// a
 
-rectangulo(altura, ancho);
+for (let i=0; i<5; i++){
+    console.log("Practicando con el bucle for" + i);
+};
+
+// b
+
+for (let i=0; i<10; i ++){
+    console.log("La variable i tiene el valor valor de ${i}" + i);
+};
+
+// c
+//i.
+
+for (let i=0; i<10; i++){
+    console.log(i * 2);
+};
+
+// para que i varie de 2 en 2 modificaria i ++ a i +=2:
+
+for (let i=0; i<10; i +=2){
+    console.log(i * 2);
+};
+
+// ii.
+
+for (let i=0; i<50; i ++){
+    console.log(i * 2);
+};
+
+// para que i varie de 5 en 5 modificaria i ++ a i +=5:
+
+for (let i=0; i<50; i += 5){
+    console.log(i * 2);
+};
+
+// d
+
+for (let i=100; 100 <= i >= 0; i ++){
+    console.log(i);
+};
+
+
+
